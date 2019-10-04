3b3b6eb534659c5538a553ecb4712b333d7a831c
Xiyu Yu <yxy_1990@hotmail.com>
2019-10-04 14:30:33 -0400

    modified search

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Map;
 
 
@@ -24,10 +27,11 @@ public class HomeController {
     @RequestMapping("/")
     public String messageList(Model model) {
         model.addAttribute("jobs", jobRepository.findAll());
-        return "index";}
+        return "index";
+    }
 
     @RequestMapping("/login")
-    public String login(){
+    public String login() {
         return "login";
     }
 
@@ -40,47 +44,60 @@ public class HomeController {
 
 
     @PostMapping("/process")
-    public String processForm(@ModelAttribute Job job, @RequestParam("file") MultipartFile file, BindingResult result){
-                            if(file.isEmpty()){
-                                job.setDefaultPic();
-                                jobRepository.save(job);
-                                return "redirect:/";
-                            }
-                            try {
-                                Map uploadResult = cloudc.upload(file.getBytes(),
-                                        ObjectUtils.asMap("resourcetype","auto"));
-                                job.setPic(uploadResult.get("url").toString());
-                                jobRepository.save(job);
-                            }catch (IOException e){
-                                e.printStackTrace();
-                                return  "redirect:/add";}
-                            if (result.hasErrors()) {
-                                return "jobform";
-                            }
-                            jobRepository.save(job);
-                            return "redirect:/";
-                        }
+    public String processForm(@ModelAttribute Job job, @RequestParam("file") MultipartFile file, BindingResult result) {
+        if (file.isEmpty()) {
+            job.setDefaultPic();
+            jobRepository.save(job);
+            return "redirect:/";
+        }
+        try {
+            Map uploadResult = cloudc.upload(file.getBytes(),
+                    ObjectUtils.asMap("resourcetype", "auto"));
+            job.setPic(uploadResult.get("url").toString());
+            jobRepository.save(job);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "redirect:/add";
+        }
+        if (result.hasErrors()) {
+            return "jobform";
+        }
+        jobRepository.save(job);
+        return "redirect:/";
+    }
 
     @RequestMapping("/detail/{id}")
-    public String showMessage(@PathVariable("id") long id, Model model){
-        model.addAttribute("job",jobRepository.findById(id).get());
+    public String showMessage(@PathVariable("id") long id, Model model) {
+        model.addAttribute("job", jobRepository.findById(id).get());
         return "show";
     }
 
     @RequestMapping("/update/{id}")
-    public String updateItem(@PathVariable("id") long id, Model model){
-        model.addAttribute("job",jobRepository.findById(id).get());
-        return "jobform";}
+    public String updateItem(@PathVariable("id") long id, Model model) {
+        model.addAttribute("job", jobRepository.findById(id).get());
+        return "jobform";
+    }
 
     @RequestMapping("/delete/{id}")
-    public String deleteItem(@PathVariable("id") long id){
+    public String deleteItem(@PathVariable("id") long id) {
         jobRepository.deleteById(id);
         return "redirect:/";
     }
+
     @PostMapping("/processsearch")
-    public String searchResult(Model model,@RequestParam(name="search") String search) {
-        model.addAttribute("jobs", jobRepository.findByContentContainingIgnoreCaseOrTitleContainingIgnoreCaseOrAuthorContainingIgnoreCase(search,search,search));
-        return "index";
+    public String searchResult(Model model, @RequestParam(name = "search") String search) {
+        String[] list = search.split(" ");
+        ArrayList<Job> jlist = new ArrayList<>();
+        HashSet<Job>  alllist = new HashSet();
+
+        for (String s : list) {
+            jlist = jobRepository.findByContentContainingIgnoreCaseOrTitleContainingIgnoreCaseOrAuthorContainingIgnoreCase(s, s, s);
+            for (Job j : jlist) {
+                alllist.add(j);
+            }
+            model.addAttribute("jobs", alllist);
+
+        }return "index";
     }
 
 }
